47b023cf56cc983375675acba82c2c8e7f190c18
sandrine <sandrinezocca@gmail.com>
2020-12-11 17:12:02 +0100

    reverse string

---
+++ README.md
@@ -0,0 +1,5 @@
+## Write a function that takes an array of characters and reverses the letters in place. ↴
+
+>Why an array of characters instead of a string?
+
+>The goal of this question is to practice manipulating strings in place. Since we're modifying the input, we need a mutable ↴ type like an array, instead of JavaScript's immutable strings.
---
+++ Reverse-string-in-place/reverse-string.js
@@ -0,0 +1,59 @@
+function reverse(arrayOfChars) {
+
+  console.log(arrayOfChars);
+
+  // Reverse the input array of characters in place
+  if (arrayOfChars.length <= 1) return
+  arrayOfChars.reverse();
+
+  console.log(arrayOfChars);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'empty string';
+let input = ''.split('');
+reverse(input);
+let actual = input.join('');
+let expected = '';
+assertEqual(actual, expected, desc);
+
+desc = 'single character string';
+input = 'A'.split('');
+reverse(input);
+actual = input.join('');
+expected = 'A';
+assertEqual(actual, expected, desc);
+
+desc = 'longer string';
+input = 'ABCDE'.split('');
+reverse(input);
+actual = input.join('');
+expected = 'EDCBA';
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
