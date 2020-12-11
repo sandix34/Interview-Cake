61d89862dbcb809d435e1c379e661957532ed7fd
sandrine <sandrinezocca@gmail.com>
2020-12-11 17:35:54 +0100

    add comment and solution

--- Reverse-string-in-place/reverse-string.js
+++ Reverse-string-in-place/reverse-string.js
@@ -1,20 +1,41 @@
+// My solution
 function reverse(arrayOfChars) {
 
-  console.log(arrayOfChars);
+  console.log(arrayOfChars); // [], [ 'A' ], [ 'A', 'B', 'C', 'D', 'E' ] 
 
   // Reverse the input array of characters in place
   if (arrayOfChars.length <= 1) return
   arrayOfChars.reverse();
 
-  console.log(arrayOfChars);
+  console.log(arrayOfChars); // [ 'E', 'D', 'C', 'B', 'A' ] 
 
 }
 
 
-
-
-
-
+/* -------
+* Solution
+* We swap the first and last characters, then the second and second-to-last characters, and so on until we reach the middle.
+* --------
+*
+* function reverse(arrayOfChars) {
+* 
+*   let leftIndex = 0;
+*   let rightIndex = arrayOfChars.length - 1;
+* 
+*   while (leftIndex < rightIndex) {
+* 
+*     // Swap characters
+*     const temp = arrayOfChars[leftIndex];
+*     arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+*     arrayOfChars[rightIndex] = temp;
+* 
+*     // Move towards middle
+*     leftIndex++;
+*     rightIndex--;
+*   }
+* }
+* 
+*/
 
 
 
